4a28f464112490e1d1d02a52423fbd9b9a29691f
an0oynihs <an0oynihs@naver.com>
2025-01-15 18:16:06 +0900

    250115

---
+++ javascript/birthflower/script/birthflower.js
@@ -0,0 +1,18 @@
+//const birthFlower = document.querySelector('');
+const month = prompt('태어난 달을 숫자만 입력해 주세요.');
+if(month >= 1 && month <= 12){
+    if(month = 1){alert('');}
+    else if(month = 2){alert('1월에 태어난 당신의 탄생화는 수선화입니다.');}
+    else if(month = 3){alert('');}
+    else if(month = 4){alert('');}
+    else if(month = 5){alert('');}
+    else if(month = 6){alert('');}
+    else if(month = 7){alert('');}
+    else if(month = 8){alert('');}
+    else if(month = 9){alert('');}
+    else if(month = 10){alert('');}
+    else if(month = 11){alert('');}
+    else(month = 12){alert('';)}
+}else{
+    alert('1부터 12 사이의 숫자를 입력해 주세요.');
+}
