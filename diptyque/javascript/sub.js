140bb96544be901493b1fd20580b56f20b95d11b
an0oynihs <an0oynihs@naver.com>
2025-02-14 20:14:56 +0900

    diptyque

--- diptyque/javascript/sub.js
+++ diptyque/javascript/sub.js
@@ -1,21 +1,10 @@
 //new item 슬라이드
-const newItem = new Swiper('.new_item',{
-    slidesPerView:5,
-    spaceBetween:25,
-    navigation:{
-        nextEl:'.new_item .slide_btn .next',
-        prevEl:'.new_item .slide_btn .prev',
-    },
-})
-
-//collection 슬라이드
-const collection = new Swiper('.collection .card_wrap',{
-    slidesPerView:3.7,
-    spaceBetween:40,
-    slidesPerGroup:1,
+const newItem = new Swiper('.new .new_item',{
+    slidesPerView:4,
+    spaceBetween:72,
     loop:true,
     navigation:{
-        nextEl:'.collection .slide_btn .next',
-        prevEl:'.collection .slide_btn .prev',
+        nextEl:'.slide_btn .next',
+        prevEl:'.slide_btn .prev',
     },
 })
