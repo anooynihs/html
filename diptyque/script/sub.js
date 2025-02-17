a18095641ab305fd834e23b96cfad11e22480276
an0oynihs <an0oynihs@naver.com>
2025-02-17 14:39:18 +0900

    diptyque

--- diptyque/script/sub.js
+++ diptyque/script/sub.js
@@ -8,3 +8,13 @@ const newItem = new Swiper('.new .new_item',{
         prevEl:'.slide_btn .prev',
     },
 })
+
+//all item more, less 버튼
+document.getElementById('load_more').addEventListener('click', function() {
+    const hiddenItems = document.querySelector('.hidden-item');
+    
+    if (hiddenItems.style.display === 'none' || hiddenItems.style.display === '') {
+        hiddenItems.style.display = 'block'; // 보이게 설정
+        this.textContent = 'NO MORE'; // 버튼 텍스트 변경
+    }
+});
